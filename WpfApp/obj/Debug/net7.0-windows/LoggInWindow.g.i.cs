09793ab28553304abfa6172516fa138eacb79f44
Ivan Savochkin <vaniasavochkin@gmail.com>
2023-09-06 16:21:16 +0300

    sss
    ss

--- WpfApp/obj/Debug/net7.0-windows/LoggInWindow.g.i.cs
+++ WpfApp/obj/Debug/net7.0-windows/LoggInWindow.g.i.cs
@@ -175,13 +175,13 @@ namespace WpfApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp;component/logginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp;V1.0.0.0;component/logginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\LoggInWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -191,7 +191,7 @@ namespace WpfApp {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
